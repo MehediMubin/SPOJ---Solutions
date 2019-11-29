811647992b2893246e498b6c6bf9244bcd20f29c
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2019-11-29 21:46:50 +0600

    Add files via upload

---
+++ PRIME1 - Prime Generator.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void PrimeGenerator (int m, int n)
+{
+    for (int i = m; i <= n; i++) {
+        if (i == 2) printf("%d\n", 2);
+        if (i == 3) printf("%d\n", 3);
+        if (i > 3) {
+            if (i % 2 != 0) {
+                bool b = true;
+                for (int j = 3; j <= sqrt(i); j++) {
+                    if (i % j == 0) {
+                        b = false;
+                        break;
+                    }
+                }
+                if (b) printf("%d\n", i);
+            }
+        }
+    }
+}
+
+int main ()
+{
+    int T, a, b;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d %d", &a, &b);
+        PrimeGenerator(a, b);
+        if (T != 0)
+            puts("");
+    }
+
+    return 0;
+}
