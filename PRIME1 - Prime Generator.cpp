dd6b35ce755c6eb1f84c005046379ef8757e69e3
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2021-08-04 08:12:47 +0600

    Update PRIME1 - Prime Generator.cpp

--- PRIME1 - Prime Generator.cpp
+++ PRIME1 - Prime Generator.cpp
@@ -1,36 +1,38 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-void PrimeGenerator (int m, int n)
-{
-    for (int i = m; i <= n; i++) {
-        if (i == 2) printf("%d\n", 2);
-        if (i == 3) printf("%d\n", 3);
-        if (i > 3) {
-            if (i % 2 != 0) {
-                bool b = true;
-                for (int j = 3; j <= sqrt(i); j += 2) {
-                    if (i % j == 0) {
-                        b = false;
-                        break;
-                    }
-                }
-                if (b) printf("%d\n", i);
-            }
-        }
-    }
-}
+#define nl "\n"
+#define ll long long
+#define pb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 int main ()
 {
-    int T, a, b;
-    scanf("%d", &T);
+    FastIO;
+
+    int T;
+    cin >> T;
+
     while (T--) {
-        scanf("%d %d", &a, &b);
-        PrimeGenerator(a, b);
-        if (T != 0)
-            puts("");
+        int a, b;
+        cin >> a >> b;
+
+        vector <int> v;
+        for (int i = a; i <= b; i++) {
+
+            if (i == 2 || i == 3) v.pb(i);
+            else if (i == 1 || i % 2 == 0) continue;
+            else {
+                bool ok = true;
+                for (int j = 3; j * j <= i; j += 2) {
+                    if (i % j == 0) ok = false;
+                }
+                if (ok) v.pb(i);
+            }
+        }
+        for (auto i : v) cout << i << nl;
     }
 
     return 0;
